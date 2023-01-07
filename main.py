38f1b2377b550bcbc4813e33316337ecb6911537
imaspacecat <library4lion@gmail.com>
2023-01-06 23:15:04 -0800

    initial commit

---
+++ main.py
@@ -0,0 +1,60 @@
+import requests as req
+from bs4 import BeautifulSoup as bs
+from dotenv import dotenv_values
+import re
+from seleniumwire import webdriver
+from selenium.webdriver.common.by import By
+from seleniumwire.utils import decode
+
+
+# submit = driver.find_element(By.ID, "solutionsubmit")
+
+
+root = "https://practiceit.cs.washington.edu"
+url_login = root + "/login"
+
+config = dotenv_values(".env")
+
+payload = {
+    'usernameoremail': config['EMAIL'],
+    'password': config['PASSWORD']
+}
+
+s = req.sessions.session()
+r = s.post(url_login, data=payload)
+
+problems_solved = s.get("https://practiceit.cs.washington.edu/user/problems-solved").text
+# print(problems_solved)
+
+links = re.findall("\/problem\/view.+(?=\")", problems_solved)
+# print(links)
+
+solution_links = []
+
+
+driver = webdriver.Chrome()
+driver.get(url_login)
+driver.find_element(By.NAME, "usernameoremail").send_keys(config['EMAIL'])
+driver.find_element(By.NAME, "password").send_keys(config['PASSWORD'])
+driver.find_element(By.ID, "submitbutton").submit()
+
+f = open("links.txt", "a")
+
+for link in links:
+    driver.get(root + link)
+    driver.find_element(By.ID, "solutionsubmit").click()
+    for r in driver.requests:
+        if r.url == "https://practiceit.cs.washington.edu/test/enqueue-job":
+            data = decode(r.body, r.headers.get('Content-Encoding', 'identity'))
+            print(
+                r.url + "/" + str(data.decode())
+            )
+            f.write(r.url + "/" + str(data.decode()))
+
+
+
+
+# for link in links:
+#     s.get(root + link).text
+
+
